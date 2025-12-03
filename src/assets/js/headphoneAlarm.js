10d207ea9bc3033fbf9c96c5f5abef0fe77826fe
GroveSigilOfficial <grovesigilofficial@gmail.com>
2025-12-03 11:50:44 -0800

    Update headphoneAlarm.js

--- src/assets/js/headphoneAlarm.js
+++ src/assets/js/headphoneAlarm.js
@@ -1,24 +1,10 @@
-let audioEl = null;
+let alarmAudio = new Audio("/sandbox-private/public/alarm.mp3");
 
-export async function playAlarm() {
-  if (!audioEl) {
-    audioEl = document.createElement('audio');
-    audioEl.src = '/public/alarm.mp3';
-    audioEl.loop = true;
-    audioEl.preload = 'auto';
-    document.body.appendChild(audioEl);
-  }
+export function playAlarm() {
+    alarmAudio.play();
+}
 
-  try {
-    await audioEl.play();
-  } catch(e) {
-    console.warn('Autoplay failed', e);
-  }
-
-  setTimeout(() => {
-    if(audioEl) {
-      audioEl.pause();
-      audioEl.currentTime = 0;
-    }
-  }, 6000);
+export function stopAlarm() {
+    alarmAudio.pause();
+    alarmAudio.currentTime = 0;
 }
