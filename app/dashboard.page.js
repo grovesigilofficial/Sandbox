c14420012bb4cc3d40ca0813aa104f71cb96526c
GroveSigilOfficial <grovesigilofficial@gmail.com>
2025-12-02 23:54:32 -0800

    Create dashboard.page.js

---
+++ app/dashboard.page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import UbermanTimer from "@/components/UbermanTimer";
+
+export default function Dashboard() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Sandbox Dashboard</h1>
+      <UbermanTimer />
+    </main>
+  );
+}
